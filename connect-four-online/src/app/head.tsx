6f5cab8f24e256a2b872ccae0bf7e4472bfae248
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-01-20 17:42:27 -0800

    INIT

---
+++ connect-four-online/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Connect Four Online</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Literally Connect Four Online" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
